119a207fa49723dc909a841b4ae75e5a6718e6c5
Mike Vong <mv910@hotmail.com>
2020-02-25 12:03:39 -0800

    added var in game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){});
+  
+  //random generate a number between 19 and 120
+        //var for number
+    var randomNumber;
+    var redGem;
+    var greenGem;
+    var blueGem;
+    var yelloGem;
+    var wins = 0;
+    var losses = 0;
+    var matchNumber;
+
+    randomNumber = Math.floor(Math.random() * 120);
+    console.log(randomNumber);
+    $(".match").html(randomNumber);
+    //random generate each gem between 1 and 12
+        //yellow gem
+        //green gem
+        //blue gem
+        //red gem
+
+
+    //variables for win and lost
+
+    //fuction to counting lost
+
+    //fuction to counting wins
+
+    //function on click on each gem
+
+        //yellow gem click
+
+        //red gem click
+
+        //green gem click
+
+        //blue gem click
+    
+    //function to reset and play again
+  
+  
+
