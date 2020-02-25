353ec94c32352b49452eb25e69e412d96837d26b
Mike Vong <mv910@hotmail.com>
2020-02-25 12:57:18 -0800

    added comments on code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,8 @@
 $(document).ready(function()
 {
   
-  //random generate a number between 19 and 120
-        //var for number
+ 
+    //var for number
     var randomNumber;
     var redGem;
     var greenGem;
@@ -10,10 +10,12 @@ $(document).ready(function()
     var yellowGem;
     var wins = 0;
     var losses = 0;
-
     var totalScore = 0;
+    //start the random number at the begin of the game
     randomNum();
-
+    
+    
+     //random generate a number between 19 and 120 and displaying it
     function randomNum()
         {
         randomNumber = Math.floor(Math.random() * 120);
@@ -22,27 +24,35 @@ $(document).ready(function()
         };
 
     
-    //random generate each gem between 1 and 12
+    
     //yellow gem
     $("#yellow").on("click",function()
         {
+            // //random generate a number between 1 and 12
             yellowGem = Math.floor(Math.random() * 12);
-            console.log(yellowGem);
+            console.log("Yellow Gem click " +yellowGem);
+            //adding up the clicks to totalScore
             totalScore = yellowGem + totalScore;
             $("#totalScore").html(totalScore);
+            //when totalScore equal to randomNumber Win
             if (totalScore === randomNumber)
                 {
+                //adding 1 to wins
                 wins++;
                 alert("You won!");
                 $("#totalWins").html(wins);
+                //reseting totalScore and random number
                 totalScore = 0;
                 randomNum();
                 }
+            //when totalScore greater than randomNumber lose
             else if (totalScore > randomNumber)
                 {
+                //adding 1 to losses
                 losses++;
                 alert("You lost!");
                 $("#totalLosses").html(losses);
+                //reseting totalScore and random number
                 totalScore = 0;
                 randomNum();
                 }
@@ -51,22 +61,28 @@ $(document).ready(function()
     $("#green").on("click",function()
         {
             greenGem = Math.floor(Math.random() * 12);
-            console.log(yellowGem);
-            totalScore = yellowGem + totalScore;
+            console.log("Green Gem click " + greenGem);
+            totalScore = greenGem + totalScore;
             $("#totalScore").html(totalScore);
+            //when totalScore equal to randomNumber Win
             if (totalScore === randomNumber)
                 {
+                //adding 1 to wins
                 wins++;
                 alert("You won!");
                 $("#totalWins").html(wins);
+                //reseting totalScore and random number
                 totalScore = 0;
                 randomNum();
                 }
+            //when totalScore greater than randomNumber lose
             else if (totalScore > randomNumber)
                 {
+                //adding 1 to losses
                 losses++;
                 alert("You lost!");
                 $("#totalLosses").html(losses);
+                //reseting totalScore and random number
                 totalScore = 0;
                 randomNum();
                 }
@@ -75,22 +91,28 @@ $(document).ready(function()
     $("#red").on("click",function()
         {
             redGem = Math.floor(Math.random() * 12);
-            console.log(yellowGem);
-            totalScore = yellowGem + totalScore;
+            console.log("Red Gem click " +redGem);
+            totalScore = redGem + totalScore;
             $("#totalScore").html(totalScore);
+            //when totalScore equal to randomNumber Win
             if (totalScore === randomNumber)
                 {
+                //adding 1 to wins
                 wins++;
                 alert("You won!");
                 $("#totalWins").html(wins);
+                //reseting totalScore and random number
                 totalScore = 0;
                 randomNum();
                 }
+            //when totalScore greater than randomNumber lose
             else if (totalScore > randomNumber)
                 {
+                //adding 1 to losses
                 losses++;
                 alert("You lost!");
                 $("#totalLosses").html(losses);
+                //reseting totalScore and random number
                 totalScore = 0;
                 randomNum();
                 }
@@ -99,22 +121,28 @@ $(document).ready(function()
     $("#blue").on("click",function()
         {
             blueGem = Math.floor(Math.random() * 12);
-            console.log(yellowGem);
-            totalScore = yellowGem + totalScore;
+            console.log("Blue Gem click " +blueGem);
+            totalScore = blueGem + totalScore;
             $("#totalScore").html(totalScore);
+            //when totalScore equal to randomNumber Win
             if (totalScore === randomNumber)
                 {
+                //adding 1 to wins
                 wins++;
                 alert("You won!");
                 $("#totalWins").html(wins);
+                //reseting totalScore and random number
                 totalScore = 0;
                 randomNum();
                 }
+            //when totalScore greater than randomNumber lose
             else if (totalScore > randomNumber)
                 {
+                //adding 1 to losses
                 losses++;
                 alert("You lost!");
                 $("#totalLosses").html(losses);
+                //reseting totalScore and random number
                 totalScore = 0;
                 randomNum();
                 }
